fa9d86b44dc16c96640860e96c5b5394b2158f46
syahdaafia <76998317+syahdaafia@users.noreply.github.com>
2022-09-06 01:44:26 +0700

    up

--- Mission2_VanillaJS/script.js
+++ Mission2_VanillaJS/script.js
@@ -1,9 +1,27 @@
+/*
+Tugas   : mission2_webdev
+Nama    : Syahda Afia Dhiya Ulhaq Tajudin
+NIM     : 201524030
+Kelas   : D4-3A
+File    : javascript
+*/
+
+window.onload = () => {
+  startSetTimeoutAnimation();
+  startAnimFrameAnimation();
+  startSetTimeoutAnimation2();
+  startAnimFrameAnimation2();
+  startSetTimeoutAnimation3();
+  startAnimFrameAnimation3();
+  myMove();
+};
+
 function myMove() {
     let id = null;
     const elem = document.getElementById("sun");   
     let pos = 0;
     clearInterval(id);
-    id = setInterval(frame, 100);
+    id = setInterval(frame, 50);
     function frame() {
       if (pos == 350) {
         clearInterval(id);
@@ -14,17 +32,11 @@ function myMove() {
       }
     }
 }
-myMove();
-
-window.onload = () => {
-    startSetTimeoutAnimation();
-    startAnimFrameAnimation();
-  };
   
   function startSetTimeoutAnimation() {
     const refreshRate = 1000 / 60;
     const maxXPosition = 400;
-    let rect = document.getElementById('cow');
+    let rect = document.getElementById('tractor');
     let speedX = 1;
     let positionX = 0;
   
@@ -39,8 +51,8 @@ window.onload = () => {
   
   function startAnimFrameAnimation() {
     const refreshRate = 1000 / 60;
-    const maxXPosition = 800;
-    let rect = document.getElementById('cow');
+    const maxXPosition = 400;
+    let rect = document.getElementById('tractor');
     let speedX = 1;
     let positionX = 0;
   
@@ -54,4 +66,75 @@ window.onload = () => {
     }
   
     window.requestAnimationFrame(step);
+  }
+
+  
+  function startSetTimeoutAnimation2() {
+    const refreshRate = 1000 / 60;
+    const maxXPosition = 100;
+    let rect = document.getElementById('grass');
+    let speedX = 1;
+    let positionX = 5;
+  
+    window.setInterval(() => {
+      positionX = positionX + speedX;
+      if (positionX > maxXPosition || positionX < 0) {
+        speedX = speedX * (-1);
+      }
+      rect.style.left = positionX + 'px';
+    }, refreshRate);
+  }
+  
+  function startAnimFrameAnimation2() {
+    const refreshRate = 1000 / 60;
+    const maxXPosition = 100;
+    let rect = document.getElementById('grass');
+    let speedX = 0.5;
+    let positionX = 5;
+  
+    function step2() {
+      positionX = positionX + speedX;
+      if (positionX > maxXPosition || positionX < 0) {
+        speedX = speedX * (-1);
+      }
+      rect.style.left = positionX + 'px';
+      window.requestAnimationFrame(step2);
+    }
+  
+    window.requestAnimationFrame(step2);
+  }
+
+  function startSetTimeoutAnimation3() {
+    const refreshRate = 1000 / 60;
+    const maxXPosition = 10;
+    let rect = document.getElementById('bird');
+    let speedX = 1;
+    let positionX = 0;
+  
+    window.setInterval(() => {
+      positionX = positionX + speedX;
+      if (positionX > maxXPosition || positionX < 0) {
+        speedX = speedX * (-1);
+      }
+      rect.style.left = positionX + 'px';
+    }, refreshRate);
+  }
+  
+  function startAnimFrameAnimation3() {
+    const refreshRate = 1000 / 60;
+    const maxXPosition = 10;
+    let rect = document.getElementById('bird');
+    let speedX = 1;
+    let positionX = 0;
+  
+    function step3() {
+      positionX = positionX + speedX;
+      if (positionX > maxXPosition || positionX < 0) {
+        speedX = speedX * (-1);
+      }
+      rect.style.left = positionX + 'px';
+      window.requestAnimationFrame(step3);
+    }
+  
+    window.requestAnimationFrame(step3);
   }
